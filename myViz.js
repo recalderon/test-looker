37feeff5a32f129ba76e881d9fef3f12e07fc2ce
Ricardo Calderón <rc.calderon30@gmail.com>
2025-04-07 10:36:43 -0300

    test

--- myViz.js
+++ myViz.js
@@ -1,9 +1,9 @@
 const myViz = {
   render: (data, config) => {
-    const container = document.createElement('div');
+    const container = document.createElement("div");
     container.innerHTML = `
-      <h2>Hello Looker 👋</h2>
-      <pre style="font-size: 12px;">${JSON.stringify(data, null, 2)}</pre>
+      <h2 style="font-family:sans-serif;">Hello from my custom Viz!</h2>
+      <pre style="font-size:12px;">${JSON.stringify(data, null, 2)}</pre>
     `;
     document.body.appendChild(container);
   }
